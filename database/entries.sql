9dff032b73b71328a9c64524d1b777be3e14f163
oussama <oussama.aitali@stud.h-da.de>
2026-08-23 01:54:47 +0200

    feat: add database entries

---
+++ database/entries.sql
@@ -0,0 +1,8 @@
+INSERT INTO `article` (`article_id`, `name`, `picture`, `price`)
+VALUES
+    (1, 'Salami', 'salami.png', 8.57),
+    (2, 'Margherita', 'margherita.png', 12.5),
+    (3, 'Hawaii', 'hawaii.png', 11.99),
+    (4, 'Tonno', 'tonno.png', 9.99),
+    (5, 'Vegetarier', 'vegetarier.png', 8.00),
+    (6, 'Schinken', 'schinken.png', 12.49);
