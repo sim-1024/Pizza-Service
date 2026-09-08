7ac8787351cde7f0e26912df6a1fdac626e26b05
oussama <oussama.aitali@stud.h-da.de>
2026-09-09 01:28:09 +0200

    feat: prepare conditional JavaScript inclusion in head

--- src/App/View/partials/head.php
+++ src/App/View/partials/head.php
@@ -3,6 +3,11 @@
 
 <head>
     <meta charset="UTF-8" />
+
+    <?php if (isset($script)): ?>
+        <script src="<?= $script ?>" defer></script>
+    <?php endif; ?>
+
     <title><?= $title ?> </title>
     <link rel="icon" type="image/png" href="../../../assets/images/logo.png">
 </head>
