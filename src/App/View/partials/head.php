f1650b0066a28d637eb26ab6b843a2738cc92fbe
oussama <oussama.aitali@stud.h-da.de>
2026-09-19 13:34:16 +0200

    feat: add base stylesheet and responsive viewport

--- src/App/View/partials/head.php
+++ src/App/View/partials/head.php
@@ -3,10 +3,13 @@
 
 <head>
     <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <?php if (isset($script)): ?>
         <script src="<?= $script ?>" defer></script>
     <?php endif; ?>
+
+    <link rel="stylesheet" href="assets/css/style.css">
 
     <title><?= $title ?> </title>
     <link rel="icon" type="image/png" href="../../../assets/images/logo.png">
